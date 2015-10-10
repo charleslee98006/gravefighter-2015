5b9bfa580433c3141c849be066ea0cbc0aaaeb9b
charles <charles.98006@gmail.com>
2015-10-10 02:18:32 -0700

    pushing local branch of Body to remote; created interface class  for the body and created wasd key for player.

---
+++ Assets/IBody.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+public interface IBody<J>{
+		void Attack(J enemy);
+		void Move();
+
+
+}
+public interface IBodydamage<T>{
+		void DamageTaken(T damage);
+}
+	
---
+++ Assets/playerBody.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBody : MonoBehaviour, IBody<GameObject>{
+	private float walkSpeed;
+	private float curSpeed;
+	private float maxSpeed;
+	void Start()
+	{
+		
+		curSpeed = 10;
+		
+	}
+	void Update(){
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * curSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * curSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += Vector3.up * curSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += Vector3.down * curSpeed * Time.deltaTime;
+		}
+		//Debug.Log("moving");
+	}
+	public void Move(){
+	}
+	public void Attack(GameObject enemy){
+
+	}
+	public void build(){
+	}
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+
+}
